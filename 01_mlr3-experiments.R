dfecc5354e7425d2abfd06874a2723bc7f6e0277
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-09-01 11:51:56 +0200

    start mlr3 implementation code

---
+++ 01_mlr3-experiments.R
@@ -0,0 +1,32 @@
+## libraries ----
+library(mlr3)
+library(mlr3spatial)
+library(mlr3learners)
+library(sf)
+
+## data import ----
+
+ds801 <- readRDS("./data/external/ds801_geochem1km.rds") |>
+  sf::st_as_sf(coords = c("latitude", "longitude"))
+chicago <- readRDS("./data/external/chicago_grid1km.rds")
+
+## data preparation ----
+
+# variable transformations
+ds801$log_pb = log1p(ds801$pb_ppm)  ## log transformation
+
+# feature variable selector (imported from tutorial)
+pb_feats <- c(readRDS("./data/external/pb.pr.vars.rds"), "hzn_depth")
+ds801_ids <- c("state", "site_id", "ID")
+
+# regression matrix
+ds801_pb = ds801[,c(ds801_ids, "log_pb", pb_feats)] |> ## select relevant cols for task
+  tidyr::drop_na()                          ## drop incomplete rows
+
+## mlr3 pipeline ----
+
+# task
+task_ds801 <- as_task_regr_st(ds801_pb, target = "log_pb")
+
+# initialise site `ID` and  groups
+task_ds801$col_roles$group = c("state", "site_id")
---
+++ README.md
@@ -0,0 +1,7 @@
+OpenGeoHub Hackathon #2
+
+Provided Resources:
+
+
+Open Questions:
+- What is the difference between `ID` and `site_id` in `ds801_geochem1km.rds`?
