86ad556e562fa1c5f417f634de4592a7377687df
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-09-01 14:12:00 +0200

    add notes on data variables and mlr3

--- 01_mlr3-experiments.R
+++ 01_mlr3-experiments.R
@@ -20,13 +20,13 @@ pb_feats <- c(readRDS("./data/external/pb.pr.vars.rds"), "hzn_depth")
 ds801_ids <- c("state", "site_id", "ID")
 
 # regression matrix
-ds801_pb = ds801[,c(ds801_ids, "log_pb", pb_feats)] |> ## select relevant cols for task
-  tidyr::drop_na()                          ## drop incomplete rows
+ds801_pb = ds801[,c("ID", "log_pb", pb_feats)] |> ## select relevant cols for task
+  tidyr::drop_na()                                     ## drop incomplete rows
 
 ## mlr3 pipeline ----
 
 # task
 task_ds801 <- as_task_regr_st(ds801_pb, target = "log_pb")
 
-# initialise site `ID` and  groups
-task_ds801$col_roles$group = c("state", "site_id")
+# initialise site `ID`
+task_ds801$col_roles$group = "site_id"
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 OpenGeoHub Hackathon #2
 
 Provided Resources:
-
-
-Open Questions:
-- What is the difference between `ID` and `site_id` in `ds801_geochem1km.rds`?
